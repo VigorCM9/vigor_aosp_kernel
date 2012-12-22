1879afe18c774654782f6b37b36438631383722c
Snuzzo <jrracinfan@gmail.com>
2012-12-21 23:30:28 -0800

    Dancedance governor
    Based off conservative, it has a higher ramp rate although
    not quite as high as on demand. Brought in the sleep routines
    similar to Wheatley.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -380,6 +380,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dancedance)
 #endif
 
 
