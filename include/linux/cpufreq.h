ad7b34eb1ff436a478440bfc5004865d710b36ca
Snuzzo <jrracinfan@gmail.com>
2013-01-12 06:06:36 -0800

    Added Badass governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,6 +383,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
 #endif
 
 
