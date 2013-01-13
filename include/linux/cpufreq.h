6af49f733438714de09389358291f16b41be1c0b
Snuzzo <jrracinfan@gmail.com>
2013-01-12 21:06:37 -0800

    Added Nightmare Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -395,6 +395,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
 #endif
 
 
