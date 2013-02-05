86a052c62ff99a22e17ff2085853da111b223376
Snuzzo <jrracinfan@gmail.com>
2013-02-05 07:20:36 -0500

    Added Scary

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -398,6 +398,9 @@ extern struct cpufreq_governor cpufreq_gov_badass;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
+extern struct cpufreq_governor cpufreq_gov_scary;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_scary)
 #endif
 
 
